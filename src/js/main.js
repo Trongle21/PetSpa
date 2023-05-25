68adf257ccce3b839deea86c93566aa2af54b954
Trongle21 <Trongleele2@gmail.com>
2023-05-25 17:25:33 +0700

    update htm css js

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,18 @@
+/** Header and Footer */
+
+$(function() {
+    // Tải phần header và chèn vào vị trí cần thiết trong trang web
+    $.get("header.html", function(data) {
+        $("#headerContainer").html(data);
+    });
+
+    // Tải phần footer và chèn vào vị trí cần thiết trong trang web
+    $.get("footer.html", function(data) {
+        $("#footerContainer").html(data);
+    });
+});
+
+
 /**Show menu navbar */
 
 document.querySelector('.header--bar').addEventListener('click', (e) => {
@@ -10,4 +25,15 @@ document.querySelector('.nav--mb_close').addEventListener('click', (e) => {
     document.querySelector('.overlay').classList.remove('show--overLay');
 });
 
-console.log(document.querySelector('.overlay'))
+/**Show Cart */
+const productCart = document.querySelector('.product--cart')
+console.log(productCart)
+document.querySelectorAll('.product--item__btn').forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+        productCart.classList.add('show--cart')
+    })
+})
+
+document.querySelector('.product--cart__info--close').addEventListener('click', (e) => {
+    productCart.classList.remove('show--cart');
+});
